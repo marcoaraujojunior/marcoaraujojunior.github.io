65f411152c00d69ea38bc4e5d934b963bded561f
Marco Antonio <marco@fidelize.com.br>
2017-01-08 14:14:03 -0200

    Add IClient-react Example with jsonstub

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,12 +26,17 @@
 
     formtatDate = function(date) {
         date = new Date(date);
-        return date.getFullYear() + '-'+
-            ('0' + (date.getMonth()+1) ).slice(-2) + '-' +
-            ('0' + date.getDate() ).slice(-2) + ' ' +
-            ('0' + date.getHours() ).slice(-2) + ':' +
-            ('0' + date.getMinutes() ).slice(-2) + ':' +
-            ('0' + date.getSeconds() ).slice(-2);
+        return date.toLocaleDateString(
+            'en-US',
+            {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit'
+            }
+        );
     };
 
     article = function (item) {
